cfcad9cde8f11ffa2717d694b73d5d53e48fe80f
Sumit So <103176491+sosumit001@users.noreply.github.com>
2023-05-06 00:07:46 +0530

    Update ApolloClient.js

--- frontent/src/ApolloClient.js
+++ frontent/src/ApolloClient.js
@@ -3,9 +3,9 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 
 const httpLink = createHttpLink({
     // <<<<<<< HEAD
-    uri: 'http://localhost:5000'
+   // uri: 'http://localhost:5000'
 
-    //uri:'https://symptomatic-pot-production.up.railway.app/' 
+    uri:'https://symptomatic-pot-production.up.railway.app/' 
 })
 
 
