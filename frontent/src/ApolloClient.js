a059dbcb45be3509cbf4490106c6521bc8280750
Sumit So <103176491+sosumit001@users.noreply.github.com>
2023-05-06 00:16:16 +0530

    Update ApolloClient.js

--- frontent/src/ApolloClient.js
+++ frontent/src/ApolloClient.js
@@ -5,7 +5,7 @@ const httpLink = createHttpLink({
     // <<<<<<< HEAD
    // uri: 'http://localhost:5000'
 
-    uri:'https://symptomatic-pot-production.up.railway.app/' 
+    uri:'https://garbage-production.up.railway.app/' 
 })
 
 
